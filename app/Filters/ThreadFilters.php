8429082dad58cbd103bf1903dfcdf847663b8c87
oleh <isntlazy@gmail.com>
2017-11-06 16:58:36 +0200

    refactoring

---
+++ app/Filters/ThreadFilters.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Filters;
+
+use App\User;
+use Illuminate\Http\Request;
+
+class ThreadFilters extends Filters
+{
+
+    protected $filters = ['by'];
+    /**
+     *
+     * Filter a query by the given username
+     * @param string $username
+     * @return mixed
+     */
+    protected function by($username)
+    {
+        $user = User::where('name', $username)->firstOrFail();
+        return $this->builder->where('user_id', $user->id);
+    }
+}
--- app/Thread.php
+++ app/Thread.php
@@ -32,4 +32,9 @@ class Thread extends Model
     {
         return "/threads/{$this->channel->slug}/{$this->id}";
     }
+
+    public function scopeFilter($query, $filters)
+    {
+        return $filters->apply($query);
+    }
 }
