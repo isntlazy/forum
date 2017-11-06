20ef1eaf315608da74b6ff42b81310de609936a6
oleh <isntlazy@gmail.com>
2017-11-06 17:46:07 +0200

    pagination added

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -3,27 +3,21 @@
 @section('content')
     <div class="container">
         <div class="row">
-            <div class="col-md-8 col-md-offset-2">
+            <div class="col-md-8">
                 <div class="panel panel-default">
                     <div class="panel-heading"><a href="#">{{ $thread->creator->name }}</a> написав: {{ $thread->title }}</div>
                     <div class="panel-body">
                         {{ $thread->body }}
                     </div>
                 </div>
-            </div>
-        </div>
 
-        <div class="row">
-            <div class="col-md-8 col-md-offset-2">
-                @foreach($thread->replies as $reply)
+                @foreach($replies as $reply)
                     @include('threads.reply')
                 @endforeach
-            </div>
-        </div>
 
-        @if (auth()->check())
-            <div class="row">
-                <div class="col-md-8 col-md-offset-2">
+                {{ $replies->links() }}
+
+                @if (auth()->check())
                     <form method="POST" action="{{ $thread->path() . '/replies' }}">
                         {{ csrf_field() }}
                         <div class="form-group">
@@ -31,10 +25,19 @@
                         </div>
                         <button type="submit" class="btn btn-default">Зберегти</button>
                     </form>
+                @else
+                    <p class="text-center">Будь ласка <a href="{{ route('login') }}">увійдіть</a></p>
+                @endif
+            </div>
+
+            <div class="col-md-4">
+                <div class="panel panel-default">
+                    <div class="panel-body">
+                        This thread was published {{ $thread->created_at->diffForHumans() }} by <a href="">{{ $thread->creator->name }} </a>, and currently has {{ $thread->replies_count }} {{ str_plural('comment', $thread->replies_count) }}.
+                    </div>
                 </div>
             </div>
-        @else
-            <p class="text-center">Будь ласка <a href="{{ route('login') }}">увійдіть</a></p>
-        @endif
+        </div>
+
     </div>
 @endsection
